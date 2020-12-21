a7bdddab13984cceec86858e3581efb6e2be66a5
Marco Antonio <marco.antonio@domain.fulltime>
2020-12-21 15:55:24 -0300

    .....

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,27 @@
 class NegociacaoController {
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert("chamei ação no controller")
+        let helper = new DateHelper();
+        let data = helper.textoParaData(this._inputData.value);
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+        console.log(helper.textoParaData(negociacao.data));
+        // adicionar a negociação em uma lista
     }
 
 }
