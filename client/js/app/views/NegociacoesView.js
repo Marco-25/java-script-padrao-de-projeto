b97f54120fe7178f32a06b464ca5e53db26b5596
Marco-25 <marco.antonio-25@outlook.com.br>
2020-12-22 07:15:12 -0200

    ...

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -37,10 +37,6 @@ class NegociacoesView {
         `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 
 }
 
